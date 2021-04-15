25c6ab079e90a2718ddb753659e37ea6a973e3f7
MAussem <Aussem.Maxwell@gmail.com(home)>
2021-04-15 13:09:43 -0500

    added new project changed styles and added a form

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       title: 'Max Aussem',
       headerLinks: [
         { title: 'Home', path: '/' },
@@ -23,9 +23,9 @@ class App extends React.Component {
         { title: 'Contact', path: '/contact' }
       ],
       home: {
-        title: 'Be the Best',
-        subTitle: 'Projects that Make a Difference',
-        text: 'Checkout my Projects Below'
+        title: 'Welcome to My Portfolio',
+        subTitle: 'Custom Websites Created for You',
+        text: 'Checkout My Projects Below'
       },
       about: {
         title: 'About Me'
@@ -35,28 +35,28 @@ class App extends React.Component {
       }
     }
   }
-  
+
   render() {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
-         
-         <Navbar className="border-bottom" bg="transparent" expand="lg">
-          <Navbar.Brand>Max Aussem</Navbar.Brand>
 
-          <Navbar.Toggle className="border-0" aria-controls="nabar-toggle" />
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-              <Link className="nav-link" to="/">Home</Link>
-              <Link className="nav-link" to="/about">About</Link>
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </Nav>
-          </Navbar.Collapse>
-         </Navbar>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Max Aussem</Navbar.Brand>
 
-         <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-         <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-         <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+            <Navbar.Toggle className="border-0" aria-controls="nabar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
           <Footer />
 
