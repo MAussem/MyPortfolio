7eae592364e379d8e81df23f533a36cb76673dbe
MAussem <Aussem.Maxwell@gmail.com(home)>
2021-04-16 02:07:04 -0500

    not sure what happened

---
+++ build/precache-manifest.4b851c27cac0483828be7366719e095e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482c273a4d1c3da1a70c5049fe59e89b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169d1c3714e4455a6fe7",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "90cd2542fd2e9230bccb",
+    "url": "/static/css/main.d94f0e10.chunk.css"
+  },
+  {
+    "revision": "169d1c3714e4455a6fe7",
+    "url": "/static/js/2.a2b829db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a2b829db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90cd2542fd2e9230bccb",
+    "url": "/static/js/main.b6dd9cde.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "ab6cd3ede67fa80e5a16c1ac5baad347",
+    "url": "/static/media/BrownsLogo.ab6cd3ed.jpg"
+  },
+  {
+    "revision": "7d7aa94dd6dfdcb2d67a0bf003999dd7",
+    "url": "/static/media/MaxProfile.7d7aa94d.png"
+  },
+  {
+    "revision": "f7e7a0519f5511b5ae272ae3ccf21a1d",
+    "url": "/static/media/flooringDepot.f7e7a051.png"
+  }
+]);
